7b4a0b4dac387db95a270257768d905abfffe7ea
sugreev <ksugreev88@gmail.com>
2023-05-08 12:15:01 +0530

    added hello world

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
